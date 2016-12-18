bdae07410ece2297b66b8e58e4b5cf6d214d32fb
Deep Aggarwal <deep.uiuc@gmail.com>
2016-12-17 19:40:52 -0600

    Cosmetic changes

--- mastermind/app.js
+++ mastermind/app.js
@@ -1,18 +1,24 @@
 //This functions initializes the board
 var initializeBoard = function initializeBoard() {
-    var numberOfInputFields = 20;
+    var numberOfInputFields = 15;
     var innerElement;
 
     var $inputTable = $(".inner-input-table");
     for(var i = 1; i <= numberOfInputFields; i++) {
-        innerElement = '<tr><td><input id="guess'+i+'" type="text" value="0000" maxlength="4" pattern="[0-9]{4}" required disabled><div class="out-white"></div><div class="out-white"></div><div class="out-white"></div><div class="out-white"></div></td></tr>';
+        innerElement = '<tr><td><input id="guess'+i+'" type="text" value="0000" maxlength="4" pattern="[0-9]{4}" required disabled><span class="out-white">&nbsp;</span><span class="out-white">&nbsp;</span><span class="out-white">&nbsp;</span><span class="out-white">&nbsp;</span></td></tr>';
         $inputTable.append(innerElement);
     }
+};
+
+var startTheGame = function startTheGame() {
+
 };
 
 //Magic starts here
 function main() {
     initializeBoard();
+
+    startTheGame();
 }
 
 $(document).ready(main());
