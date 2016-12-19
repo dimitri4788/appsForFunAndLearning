2771a23f72dc2e23a3e5fa357529c1ca3dc48fa7
Deep Aggarwal <deep.uiuc@gmail.com>
2016-12-18 22:42:23 -0600

    Updated more styling. Removed dynamic input fields creation and put them in html file

--- mastermind/app.js
+++ mastermind/app.js
@@ -1,24 +1,79 @@
-//This functions initializes the board
-var initializeBoard = function initializeBoard() {
-    var numberOfInputFields = 20;
-    var innerElement;
+//Game class; controls the game
+function Game() {
+    this.number = (Math.random()).toString().slice(2,6);
+    this.lastGuessCounter = 1;
 
-    var $inputTable = $(".inner-input-table");
-    for(var i = 1; i <= numberOfInputFields; i++) {
-        innerElement = '<tr><td><input id="guess'+i+'" type="text" value="0000" maxlength="4" pattern="[0-9]{4}" required disabled><span class="out-white">&nbsp;</span><span class="out-white">&nbsp;</span><span class="out-white">&nbsp;</span><span class="out-white">&nbsp;</span></td></tr>';
-        $inputTable.append(innerElement);
-    }
-};
+    //Generate random number
+    this.generateNextNumber = function generateNextNumber() {
+        return (Math.random()).toString().slice(2,6);
+    };
 
-var startTheGame = function startTheGame() {
+    //This function resumes the game
+    this.resumeGame = function resumeGame() {
+        $(".how-to-play-descp").hide()
+        $("#container").show()
+        $("#guess1").focus();
+    };
 
-};
+    //This function resets the game
+    this.resetGame = function resetGame() {
+
+    };
+
+    //This function sets the next input
+    this.nextInput = function nextInput() {
+        $("#guess" + this.lastGuessCounter).prop("disabled", false);
+        $("#guess" + this.lastGuessCounter).focus();
+    };
+}
+
+//Board class
+function Board() {
+    //Initialize the game
+    this.initializeBoard = function initializeBoard() {
+        $("#guess1").prop("disabled", false);
+        $("#guess1").focus();
+    };
+
+    //This function toggets
+    this.toggleHelp = function toggleHelp(event) {
+        $(".how-to-play-descp, #container").toggle()
+        $("#guess1").focus();
+    };
+}
 
 //Magic starts here
 function main() {
-    initializeBoard();
+    var board = new Board();
+    board.initializeBoard();
 
-    startTheGame();
+    var game = new Game();
+    console.log(game.number); //XXX
+
+    //Bind all the events
+    $(".how-to-play").click(board.toggleHelp);
+    $(".resume-game").click(game.resumeGame);
+
+    //Enter keyup should read the entered guess
+    $(document).keyup(function(event) {
+        //Check for enter key (code: 13) pressed
+        if(event.which === 13) {
+            //Get the guessed number
+            var $currInputField =  $("#guess" + game.lastGuessCounter);
+            var guessedNumber = $currInputField.val();
+            console.log("game.lastGuessCounter: " + game.lastGuessCounter);
+            console.log("guessed number: " + guessedNumber);
+
+            //Disable current input field
+            $currInputField.prop("disabled", true);
+
+            //Increase the guess counter
+            game.lastGuessCounter += 1;
+
+            //Get ready for the next input
+            game.nextInput();
+        }
+    });
 }
 
 $(document).ready(main());
