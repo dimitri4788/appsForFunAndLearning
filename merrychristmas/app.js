9342882b348967de19b5d0f1ed0f89778fbffe6b
Deep Aggarwal <deep.uiuc@gmail.com>
2016-12-25 01:03:17 -0600

    Added christmas tree. Updated button styles. Added javascript code to automatically rotate memes.

---
+++ merrychristmas/app.js
@@ -0,0 +1,30 @@
+﻿//Magic starts here
+function main() {
+    var currMemeIndex = 0,
+    allMemesDivs = $(".meme-slider div"),
+    numOfMemes = allMemesDivs.length;
+
+    function showMeme() {
+        //Get the current meme div
+        var currMemeDiv = $(".meme-slider div").eq(currMemeIndex);
+
+        //Hide all the memes divs
+        allMemesDivs.hide();
+
+        //Display the current meme
+        currMemeDiv.css("display","inline-block");
+    }
+
+    //Automatically rotate between the memes every 3 seconds
+    setInterval(function() {
+        currMemeIndex += 1;
+        if(currMemeIndex > (numOfMemes - 1)) {
+            currMemeIndex = 0;
+        }
+
+        //3 seconds are over, show the next meme
+        showMeme();
+    }, 3000);
+}
+
+$(document).ready(main());
